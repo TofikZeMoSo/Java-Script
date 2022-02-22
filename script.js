861512fa3f1b11e7df193b489d23bdf349b6b417
TofikZeMoSo <tofik.maniyar@zemosolabs.com>
2022-02-22 14:23:25 +0530

    Hey

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Tofik")
+console.clear
+function No_Sir(value){
+    return 'Hello Sir';
+}
+function Yes_Sir(func){
+    alert(func());
+}
+
+const Names = [
+    'Tofik',
+    'Maniyar',
+  ];
+  
+  console.log(Names.map(Names => Names.length));
